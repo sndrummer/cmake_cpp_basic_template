2ebd6150a8052b104a277f270936d7ae553a379a
Samuel Nuttall <sndrummer@gmail.com>
2022-04-24 11:42:41 -0600

    Make main include bits/stdc++.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Cmake C++ Quick Project" << std::endl;
+  cout << "Cmake C++ Quick Project" << endl;
 }
