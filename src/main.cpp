5e11c1a948f6215d9e17fe8f0fd6ff06eb999678
Samuel Nuttall <sndrummer@gmail.com>
2022-07-25 12:53:14 -0600

    Add stdincludes file

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+@INCLUDE@
 
 int main()
 {
---
+++ src/stdincludes.h
@@ -0,0 +1,23 @@
+/// @file Standard includes for quick builds
+
+#pragma once
+
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstdint>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stdint.h>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+
